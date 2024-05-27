8f69f4a1504f83a8b6f2be136594231dcb6e5aae
Cuong312004 <quoccuongluu03@gmail.com>
2024-05-27 22:30:19 +0700

    La4

---
+++ Lab4/Lab4/Menu.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab4
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBai7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBai7
+            // 
+            this.btnBai7.Location = new System.Drawing.Point(465, 237);
+            this.btnBai7.Name = "btnBai7";
+            this.btnBai7.Size = new System.Drawing.Size(154, 49);
+            this.btnBai7.TabIndex = 0;
+            this.btnBai7.Text = "Bài 7";
+            this.btnBai7.UseVisualStyleBackColor = true;
+            this.btnBai7.Click += new System.EventHandler(this.btnBai7_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBai7);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBai7;
+    }
+}
+
---
+++ Lab4/Lab4/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Menu : Form
+    {
+        Bai7 bai7 = new Bai7(); 
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBai7_Click(object sender, EventArgs e)
+        {
+            bai7.Show();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
