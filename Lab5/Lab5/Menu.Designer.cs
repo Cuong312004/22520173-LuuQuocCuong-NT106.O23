0f18e13f585425fe55f9f068b95de9bad9fe0dd9
Cuong312004 <quoccuongluu03@gmail.com>
2024-06-09 18:21:59 +0700

    lab5_1_2

---
+++ Lab5/Lab5/Menu.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Lab5
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBai1 = new System.Windows.Forms.Button();
+            this.btnBai2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBai1
+            // 
+            this.btnBai1.Location = new System.Drawing.Point(77, 56);
+            this.btnBai1.Name = "btnBai1";
+            this.btnBai1.Size = new System.Drawing.Size(138, 51);
+            this.btnBai1.TabIndex = 0;
+            this.btnBai1.Text = "Bài 1";
+            this.btnBai1.UseVisualStyleBackColor = true;
+            this.btnBai1.Click += new System.EventHandler(this.btnBai1_Click);
+            // 
+            // btnBai2
+            // 
+            this.btnBai2.Location = new System.Drawing.Point(77, 165);
+            this.btnBai2.Name = "btnBai2";
+            this.btnBai2.Size = new System.Drawing.Size(138, 51);
+            this.btnBai2.TabIndex = 1;
+            this.btnBai2.Text = "Bài 2";
+            this.btnBai2.UseVisualStyleBackColor = true;
+            this.btnBai2.Click += new System.EventHandler(this.btnBai2_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBai2);
+            this.Controls.Add(this.btnBai1);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBai1;
+        private System.Windows.Forms.Button btnBai2;
+    }
+}
+
---
+++ Lab5/Lab5/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Menu : Form
+    {
+
+        Bai1 bai1 = new Bai1(); 
+        Bai2 bai2 = new Bai2();
+
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBai1_Click(object sender, EventArgs e)
+        {
+            bai1.Show();
+        }
+
+        private void btnBai2_Click(object sender, EventArgs e)
+        {
+            bai2.Show();
+        }
+    }
+}
